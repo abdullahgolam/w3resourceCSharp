a80a0b7bdca80ddad5f7dd859ef547b1d4277d71
Abdullah A. Golam <abdullahagolam@gmail.com>
2020-11-09 10:49:08 +0300

    Day1

---
+++ CSharpBasic/PrintHelloAndYourName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Write a C# Sharp program to print Hello and your name in a separate line. Go to the editor 
+ * Expected Output :
+ * Hello: Alexandra Abramov
+ */
+
+namespace PrintHelloAndYourName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Fullname = "Abdullah GOLAM";
+            Console.WriteLine("Hello: {0}", Fullname);
+            Console.ReadKey();
+        }
+    }
+}
