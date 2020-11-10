cad4f9c1abb4b57ce1fd0ef2a69efbd09f12d09b
Abdullah A. Golam <abdullahagolam@gmail.com>
2020-11-10 09:10:54 +0300

    day2
    basic exersices

---
+++ CSharpBasic/printTheResult/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 4. Write a C# Sharp program to print the result of the specified operations. Go to the editor
+ * Test data:
+ * -1 + 4 * 6
+ * ( 35+ 5 ) % 7
+ * 14 + -4 * 6 / 11
+ * 2 + 15 / 6 * 1 - 7 % 2
+ */
+
+namespace printTheResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = -1 + 4 * 6;
+            int y = (35 + 5) % 7;
+            int z = 14 + -4 * 6 / 11;
+            int s = 2 + 15 / 6 * 1 - 7 % 2;
+
+            Console.WriteLine(" x = {0}\n y = {1}\n z = {2}\n s = {3}\n", x, y, z, s);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpBasic/printTheResultOfDividingTwoNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 3. Write a C# Sharp program to print the result of dividing two numbers
+ */
+namespace printTheResultOfDividingTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 5;
+            int r = num1 / num2;
+            Console.WriteLine("Division of {0} and {1} = {2} ", num1, num2, r);
+
+            Console.ReadKey();
+        }
+    }
+}
